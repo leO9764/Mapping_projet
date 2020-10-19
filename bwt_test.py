d59c21735ab51306e75b1f408c375b463ed5777d
ZacksAll <zacharyallouche@gmail.com>
2020-10-19 16:26:02 +0200

    Tout le reste

--- bwt_test.py
+++ bwt_test.py
@@ -20,3 +20,37 @@ def bwt(t):
 
 
 bwt(ref)
+
+
+def rankBwt(bw):
+''' Retourne les rangs ''' 
+    tots = dict()
+    ranks = []
+    for c in bw:
+        if c not in tots:
+            tots[c] = 0
+        ranks.append(tots[c])
+        tots[c] += 1
+return ranks, tots
+
+
+def firstCol(tots):
+''' retourne la premiere colonne ''' 
+    first = {}
+    totc = 0
+    for c, count in sorted(tots.iteritems()):
+        first[c] = (totc, totc + count)
+        totc += count
+    return first
+
+def reverseBwt(bw):
+''' Retourne le texte original de la transformation bw '''
+    ranks, tots = rankBwt(bw)
+    first = firstCol(tots)
+    rowi = 0
+    t = "$"
+    while bw[rowi] != '$':
+        c = bw[rowi]
+        t = c + t
+        rowi = first[c][0] + ranks[rowi]
+return t
