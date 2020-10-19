b73d97cbdc14da0e0fc30c52871f12a3ec54c9db
ZacksAll <zacharyallouche@gmail.com>
2020-10-19 16:13:36 +0200

    Test ajouté

---
+++ bwt_test.py
@@ -0,0 +1,23 @@
+ref = "ATATCGT"
+
+def suffixArray(s):
+	''' creation du suffixe array avec leurs rangs ordonnés ''' 
+	satups = sorted([(s[i:], i) for i in range(0, len(s)+1)])
+	print(map(lambda x: x[1], satups))
+	return map(lambda x: x[1], satups)
+
+suffixArray(ref)
+
+def bwt(t):
+    ''' transformation de Burrow-wheeler ''' 
+    bw = []
+    for si in suffixArray(t):
+        if si == 0:
+            bw.append('$')
+        else:
+            bw.append(t[si-1])
+    print(''.join(bw))
+    return ''.join(bw)
+
+
+bwt(ref)
