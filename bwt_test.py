a529c710215f51a2dd0b6a6a6f5f223d5283bb90
Léo MAury <leomaury@pop-os.localdomain>
2020-10-19 17:50:01 +0200

    few mod

--- bwt_test.py
+++ bwt_test.py
@@ -9,6 +9,7 @@ suffixArray(ref)
 
 def bwt(t):
     ''' transformation de Burrow-wheeler ''' 
+    global bw
     bw = []
     for si in suffixArray(t):
         if si == 0:
@@ -23,34 +24,45 @@ bwt(ref)
 
 
 def rankBwt(bw):
-''' Retourne les rangs ''' 
-    tots = dict()
-    ranks = []
-    for c in bw:
-        if c not in tots:
-            tots[c] = 0
-        ranks.append(tots[c])
-        tots[c] += 1
-return ranks, tots
+	''' Retourne les rangs ''' 
+	global tots
+	tots = dict()
+	ranks = []
+	for c in bw:
+		if c not in tots:
+			tots[c] = 0
+			ranks.append(tots[c])
+		tots[c] += 1
+	return ranks, tots
+
+
+rankBwt(''.join(bw))
 
 
 def firstCol(tots):
-''' retourne la premiere colonne ''' 
-    first = {}
-    totc = 0
-    for c, count in sorted(tots.iteritems()):
-        first[c] = (totc, totc + count)
-        totc += count
-    return first
+	''' retourne la premiere colonne ''' 
+	first = {}
+	totc = 0
+	for c, count in sorted(tots.iteritems()):
+		first[c] = (totc, totc + count)
+		totc += count
+	return first
+
+
+firstCol(tots)
+
 
 def reverseBwt(bw):
-''' Retourne le texte original de la transformation bw '''
-    ranks, tots = rankBwt(bw)
-    first = firstCol(tots)
-    rowi = 0
-    t = "$"
-    while bw[rowi] != '$':
-        c = bw[rowi]
-        t = c + t
-        rowi = first[c][0] + ranks[rowi]
-return t
+	''' Retourne le texte original de la transformation bw '''
+	ranks, tots = rankBwt(bw)
+	first = firstCol(tots)
+	rowi = 0
+	t = "$"
+	while bw[rowi] != '$':
+		c = bw[rowi]
+		t = c + t
+		rowi = first[c][0] + ranks[rowi]
+	return t
+
+
+reverseBwt(ref)
